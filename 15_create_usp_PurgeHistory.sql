a249b32d81c71894daaedf6eed0fc2490b4ffcbe
Zdenek Korvas <korvasz@seznam.cz>
2026-08-14 19:47:17 +0200

    feat: prepare it for job

--- 15_create_usp_PurgeHistory.sql
+++ 15_create_usp_PurgeHistory.sql
@@ -2,18 +2,31 @@ USE [DBA]
 GO
 
 CREATE OR ALTER PROCEDURE [Maintenance].[usp_PurgeHistory]
-    @jobHistoryDays INT = 30,
-    @backupHistoryDays INT = 30
+    @jobHistoryDays INT = 60,
+    @backupHistoryDays INT = 60
 AS
 BEGIN
     
     SET NOCOUNT ON;
     DECLARE @jobCut DATETIME = DATEADD(DAY, -@jobHistoryDays, GETDATE());
     DECLARE @backupCut DATETIME = DATEADD(DAY, -@backupHistoryDays, GETDATE());
+    DECLARE @ErrorMessage NVARCHAR(MAX);
 
-    -- Purge Job History
-    EXEC msdb.dbo.sp_purge_jobhistory @oldest_date = @jobCut;
+    BEGIN TRY
+        -- Purge Job History
+        EXEC msdb.dbo.sp_purge_jobhistory @oldest_date = @jobCut;
 
-    -- Purge Backup History
-    EXEC msdb.dbo.sp_delete_backuphistory @oldest_date = @backupCut;
+        -- Purge Backup History
+        EXEC msdb.dbo.sp_delete_backuphistory @oldest_date = @backupCut;
+    END TRY
+    BEGIN CATCH
+        SET @ErrorMessage = ERROR_MESSAGE();
+        EXEC msdb.dbo.sp_send_dbmail
+            @profile_name = 'HomeMonitoringAlerts',
+            @recipients = 'zancroweq@gmail.com',
+            @subject = 'Purge History Failed',
+            @body =  @ErrorMessage;                     
+
+        THROW;
+    END CATCH
 END

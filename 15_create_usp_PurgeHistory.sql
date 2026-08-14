1d1f6968079f680f1d42f5e0a1cc370cc52bccf0
Zdenek Korvas <korvasz@seznam.cz>
2026-08-14 19:29:29 +0200

    feat: proc for deleting jobhistory,backuphistory

---
+++ 15_create_usp_PurgeHistory.sql
@@ -0,0 +1,19 @@
+USE [DBA]
+GO
+
+CREATE OR ALTER PROCEDURE [Maintenance].[usp_PurgeHistory]
+    @jobHisstoryDays INT = 30,
+    @backupHistoryDays INT = 30
+AS
+BEGIN
+    
+    SET NOCOUNT ON;
+    DECLARE @jobCut DATETIME = DATEADD(DAY, -@jobHisstoryDays, GETDATE());
+    DECLARE @backupCut DATETIME = DATEADD(DAY, -@backupHistoryDays, GETDATE());
+
+    -- Purge Job History
+    EXEC msdb.dbo.sp_purge_jobhistory @oldest_date = @jobCut;
+
+    -- Purge Backup History
+    EXEC msdb.dbo.sp_delete_backuphistory @oldest_date = @backupCut;
+END
